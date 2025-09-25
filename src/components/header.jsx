a189ee80ea8c75beb12455179556bee9ec0a0197
Dev Suthar <4ev3rdk@gmail.com>
2025-09-25 23:48:47 +0530

    Created Header and Added List and Card to Main

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 const Header = () => {
 
     return (
-        <div className="bg-[#1d2125]"> {/* we use brackets because of custom colors or else we could use bg-yellow-500 directly */}
-            header
+        <div className="bg-[#1d2125] w-full h-12 p-3 border-b border-box flex flex-row justify-between"> {/* we use brackets because of custom colors or else we could use bg-yellow-500 directly */}
+            <div className="left justify-center flex items-center "><h3 className="text-slate-50">Taskify</h3></div>
+
+            <div className="right flex items-center space-x-4">
+                <span>Remote Dev </span>
+                <img className="rounded-full" src="https://picsum.photos/id/237/200/300 " height="40" width="40" alt="" />
+
+            </div>
         </div>
     );
 }
